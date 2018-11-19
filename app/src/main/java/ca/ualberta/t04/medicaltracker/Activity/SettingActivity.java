fcc70f85edf9bf49ec8c6c6756a475306e6020f6
Yuan Wang <wang17@ualberta.ca>
2018-11-18 20:35:11 -0700

    update setting page

--- app/src/main/java/ca/ualberta/t04/medicaltracker/Activity/SettingActivity.java
+++ app/src/main/java/ca/ualberta/t04/medicaltracker/Activity/SettingActivity.java
@@ -12,24 +12,10 @@ import ca.ualberta.t04.medicaltracker.R;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private EditText name_edit_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-        name_edit_text = findViewById(R.id.setting_name_editText);
-        name_edit_text.setText(DataController.getUser().getName());
-    }
-
-    public void apply(View view)
-    {
-        String newName = name_edit_text.getText().toString();
-        if(!newName.equals(""))
-            DataController.getUser().setName(newName);
-        else
-            Toast.makeText(SettingActivity.this, "Your name cannot be empty!", Toast.LENGTH_SHORT).show();
-        finish();
     }
 
     public void changePassword(View view){
