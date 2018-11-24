a2441150b4ffe5e0b426b79bfa29cefe75b19193
Yuan Wang <wang17@ualberta.ca>
2018-11-24 12:06:14 -0700

    update language setting

---
+++ app/src/main/java/ca/ualberta/t04/medicaltracker/Activity/CountryLanguage.java
@@ -0,0 +1,13 @@
+package ca.ualberta.t04.medicaltracker.Activity;
+
+public class CountryLanguage {
+    int resId;
+    String countryName;
+
+    public CountryLanguage(int resId, String countryName) {
+        this.resId = resId;
+        this.countryName = countryName;
+    }
+
+
+}
